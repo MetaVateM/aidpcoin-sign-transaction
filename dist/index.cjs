60055937c321bc5ea8d49ec4b8ac82849b188545
MetaVateM <loloadsl@hotmail.fr>
2024-09-11 21:02:54 +0000

    Update AIDP

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,48 +1,55 @@
-var $74Dkp$buffer = require("buffer");
-var $74Dkp$hyperbitjschains = require("@hyperbitjs/chains");
-var $74Dkp$bitcoinjslib = require("bitcoinjs-lib");
+var $g5Y9E$buffer = require("buffer");
+var $g5Y9E$bitcoinjslib = require("bitcoinjs-lib");
+var $g5Y9E$hyperbitjschains = require("@hyperbitjs/chains");
 
 
-var $80bd448eb6ea085b$require$Buffer = $74Dkp$buffer.Buffer;
-"use strict";
-Object.defineProperty(module.exports, "__esModule", {
-    value: true
-});
-module.exports["default"] = void 0;
-module.exports.sign = $80bd448eb6ea085b$var$sign;
+function $parcel$defineInteropFlag(a) {
+  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
+}
+
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$defineInteropFlag(module.exports);
+
+$parcel$export(module.exports, "sign", () => $80bd448eb6ea085b$export$c5552dfdbc7cec71);
+$parcel$export(module.exports, "default", () => $80bd448eb6ea085b$export$2e2bcd8739ae039);
 
 
-function $80bd448eb6ea085b$var$sign(network, rawTransactionHex, UTXOs, privateKeys) {
-    var networkMapper = {
-        aidp: $74Dkp$hyperbitjschains.chains.aidp.main,
-        "aidp-test": $74Dkp$hyperbitjschains.chains.aidp.test,
-        evr: $74Dkp$hyperbitjschains.chains.evr.main,
-        "evr-test": $74Dkp$hyperbitjschains.chains.evr.test
+var $80bd448eb6ea085b$require$Buffer = $g5Y9E$buffer.Buffer;
+
+function $80bd448eb6ea085b$export$c5552dfdbc7cec71(network, rawTransactionHex, UTXOs, privateKeys) {
+    const networkMapper = {
+        aidp: (0, $g5Y9E$hyperbitjschains.chains).aidp.main,
+        "aidp-test": (0, $g5Y9E$hyperbitjschains.chains).aidp.test,
+        evr: (0, $g5Y9E$hyperbitjschains.chains).evr.main,
+        "evr-test": (0, $g5Y9E$hyperbitjschains.chains).evr.test
     };
-    var coin = networkMapper[network];
+    const coin = networkMapper[network];
     if (!coin) throw new Error("Validation error, first argument network must be aidp, aidp-test, evr or evr-test");
     //@ts-ignore
-    var AIDPCOIN = (0, $74Dkp$hyperbitjschains.toBitcoinJS)(coin);
-    var tx = $74Dkp$bitcoinjslib.Transaction.fromHex(rawTransactionHex);
-    var txb = $74Dkp$bitcoinjslib.TransactionBuilder.fromTransaction(tx, AIDPCOIN);
+    const AIDPCOIN = (0, $g5Y9E$hyperbitjschains.toBitcoinJS)(coin);
+    const tx = $g5Y9E$bitcoinjslib.Transaction.fromHex(rawTransactionHex);
+    const txb = $g5Y9E$bitcoinjslib.TransactionBuilder.fromTransaction(tx, AIDPCOIN);
     function getKeyPairByAddress(address) {
-        var wif = privateKeys[address];
-        var keyPair = $74Dkp$bitcoinjslib.ECPair.fromWIF(wif, AIDPCOIN);
+        const wif = privateKeys[address];
+        const keyPair = $g5Y9E$bitcoinjslib.ECPair.fromWIF(wif, AIDPCOIN);
         return keyPair;
     }
     function getUTXO(transactionId, index) {
-        return UTXOs.find(function(utxo) {
+        return UTXOs.find((utxo)=>{
             return utxo.txid === transactionId && utxo.outputIndex === index;
         });
     }
-    for(var i = 0; i < tx.ins.length; i++){
-        var input = tx.ins[i];
-        var txId = $80bd448eb6ea085b$require$Buffer.from(input.hash, "hex").reverse().toString("hex");
-        var utxo = getUTXO(txId, input.index);
+    for(let i = 0; i < tx.ins.length; i++){
+        const input = tx.ins[i];
+        const txId = $80bd448eb6ea085b$require$Buffer.from(input.hash, "hex").reverse().toString("hex");
+        const utxo = getUTXO(txId, input.index);
         if (!utxo) throw Error("Could not find UTXO for input " + input);
-        var address = utxo.address;
-        var keyPair = getKeyPairByAddress(address);
-        var signParams = {
+        const address = utxo.address;
+        const keyPair = getKeyPairByAddress(address);
+        const signParams = {
             prevOutScriptType: "p2pkh",
             vin: i,
             keyPair: keyPair,
@@ -50,11 +57,11 @@ function $80bd448eb6ea085b$var$sign(network, rawTransactionHex, UTXOs, privateKe
         };
         txb.sign(signParams);
     }
-    var signedTxHex = txb.build().toHex();
+    const signedTxHex = txb.build().toHex();
     return signedTxHex;
 }
-var $80bd448eb6ea085b$var$_default = module.exports["default"] = {
-    sign: $80bd448eb6ea085b$var$sign
+var $80bd448eb6ea085b$export$2e2bcd8739ae039 = {
+    sign: $80bd448eb6ea085b$export$c5552dfdbc7cec71
 };
 
 
--- dist/index.mjs
+++ dist/index.mjs
@@ -1,49 +1,42 @@
-import {Buffer as $5n8iY$Buffer} from "buffer";
-import {chains as $5n8iY$chains, toBitcoinJS as $5n8iY$toBitcoinJS} from "@hyperbitjs/chains";
-import {Transaction as $5n8iY$Transaction, TransactionBuilder as $5n8iY$TransactionBuilder, ECPair as $5n8iY$ECPair} from "bitcoinjs-lib";
-
-var $c3f6c693698dc7cd$exports = {};
-
-var $c3f6c693698dc7cd$require$Buffer = $5n8iY$Buffer;
-"use strict";
-Object.defineProperty($c3f6c693698dc7cd$exports, "__esModule", {
-    value: true
-});
-$c3f6c693698dc7cd$exports["default"] = void 0;
-$c3f6c693698dc7cd$exports.sign = $c3f6c693698dc7cd$var$sign;
+import {Buffer as $hCgyA$Buffer} from "buffer";
+import {Transaction as $hCgyA$Transaction, TransactionBuilder as $hCgyA$TransactionBuilder, ECPair as $hCgyA$ECPair} from "bitcoinjs-lib";
+import {chains as $hCgyA$chains, toBitcoinJS as $hCgyA$toBitcoinJS} from "@hyperbitjs/chains";
 
 
-function $c3f6c693698dc7cd$var$sign(network, rawTransactionHex, UTXOs, privateKeys) {
-    var networkMapper = {
-        aidp: $5n8iY$chains.aidp.main,
-        "aidp-test": $5n8iY$chains.aidp.test,
-        evr: $5n8iY$chains.evr.main,
-        "evr-test": $5n8iY$chains.evr.test
+
+var $c3f6c693698dc7cd$require$Buffer = $hCgyA$Buffer;
+
+function $c3f6c693698dc7cd$export$c5552dfdbc7cec71(network, rawTransactionHex, UTXOs, privateKeys) {
+    const networkMapper = {
+        aidp: (0, $hCgyA$chains).aidp.main,
+        "aidp-test": (0, $hCgyA$chains).aidp.test,
+        evr: (0, $hCgyA$chains).evr.main,
+        "evr-test": (0, $hCgyA$chains).evr.test
     };
-    var coin = networkMapper[network];
+    const coin = networkMapper[network];
     if (!coin) throw new Error("Validation error, first argument network must be aidp, aidp-test, evr or evr-test");
     //@ts-ignore
-    var AIDPCOIN = (0, $5n8iY$toBitcoinJS)(coin);
-    var tx = $5n8iY$Transaction.fromHex(rawTransactionHex);
-    var txb = $5n8iY$TransactionBuilder.fromTransaction(tx, AIDPCOIN);
+    const AIDPCOIN = (0, $hCgyA$toBitcoinJS)(coin);
+    const tx = $hCgyA$Transaction.fromHex(rawTransactionHex);
+    const txb = $hCgyA$TransactionBuilder.fromTransaction(tx, AIDPCOIN);
     function getKeyPairByAddress(address) {
-        var wif = privateKeys[address];
-        var keyPair = $5n8iY$ECPair.fromWIF(wif, AIDPCOIN);
+        const wif = privateKeys[address];
+        const keyPair = $hCgyA$ECPair.fromWIF(wif, AIDPCOIN);
         return keyPair;
     }
     function getUTXO(transactionId, index) {
-        return UTXOs.find(function(utxo) {
+        return UTXOs.find((utxo)=>{
             return utxo.txid === transactionId && utxo.outputIndex === index;
         });
     }
-    for(var i = 0; i < tx.ins.length; i++){
-        var input = tx.ins[i];
-        var txId = $c3f6c693698dc7cd$require$Buffer.from(input.hash, "hex").reverse().toString("hex");
-        var utxo = getUTXO(txId, input.index);
+    for(let i = 0; i < tx.ins.length; i++){
+        const input = tx.ins[i];
+        const txId = $c3f6c693698dc7cd$require$Buffer.from(input.hash, "hex").reverse().toString("hex");
+        const utxo = getUTXO(txId, input.index);
         if (!utxo) throw Error("Could not find UTXO for input " + input);
-        var address = utxo.address;
-        var keyPair = getKeyPairByAddress(address);
-        var signParams = {
+        const address = utxo.address;
+        const keyPair = getKeyPairByAddress(address);
+        const signParams = {
             prevOutScriptType: "p2pkh",
             vin: i,
             keyPair: keyPair,
@@ -51,13 +44,13 @@ function $c3f6c693698dc7cd$var$sign(network, rawTransactionHex, UTXOs, privateKe
         };
         txb.sign(signParams);
     }
-    var signedTxHex = txb.build().toHex();
+    const signedTxHex = txb.build().toHex();
     return signedTxHex;
 }
-var $c3f6c693698dc7cd$var$_default = $c3f6c693698dc7cd$exports["default"] = {
-    sign: $c3f6c693698dc7cd$var$sign
+var $c3f6c693698dc7cd$export$2e2bcd8739ae039 = {
+    sign: $c3f6c693698dc7cd$export$c5552dfdbc7cec71
 };
 
 
-export {$c3f6c693698dc7cd$exports as default};
+export {$c3f6c693698dc7cd$export$c5552dfdbc7cec71 as sign, $c3f6c693698dc7cd$export$2e2bcd8739ae039 as default};
 //# sourceMappingURL=index.mjs.map
